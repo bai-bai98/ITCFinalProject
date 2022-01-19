30bc37bc1bb9a8c5d6c9e59c473f7a4c297cd34c
RoySchl <roschleyen@gmail.com>
2022-01-19 15:14:54 +0200

    homePage

--- FE/src/components/home/Home.js
+++ FE/src/components/home/Home.js
@@ -1,7 +1,11 @@
 import React from 'react';
-
+import './home.css'
 export default function Home() {
-    return <div>
-        Welcome to CleanUp!
+
+    return <div className="homeContainer">
+        <div className="homeWelcomeMessage">
+            <h1>Welcome!</h1>
+            <p>Some generic text...</p>
+        </div>
     </div>;
 }
